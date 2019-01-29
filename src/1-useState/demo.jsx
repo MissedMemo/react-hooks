cb9e2ba900ba2eb8b4c68690ab34a40330c44c8f
AndyL <missedmemo@gmail.com>
2019-01-29 13:44:48 -0800

    simplest working hooks demo

--- src/1-useState/demo.jsx
+++ src/1-useState/demo.jsx
@@ -1,3 +1,16 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
-export default ({greeting}) => <Fragment>{ greeting }</Fragment>
+const Counter = () => {
+
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount( count +1 )
+
+  return <button onClick={ increment }>{ `You clicked ${count} times so far...` }</button>
+}
+
+const App = () => <Fragment>
+  <Counter />
+</Fragment>
+
+export default App
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { render } from 'react-dom'
 import App from './1-useState/demo'
 
 render(
-  <App greeting='Hello, React Hooks!' />,
+  <App />,
   document.getElementById('root')
 )
