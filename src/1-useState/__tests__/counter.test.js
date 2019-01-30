5a81b900572cff85f11877e57f9ed6556bab61b3
AndyL <missedmemo@gmail.com>
2019-01-30 10:33:58 -0800

    apply click event test to hooks demo

--- src/1-useState/__tests__/counter.test.js
+++ src/1-useState/__tests__/counter.test.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import Counter from '../counter.jsx'
 
-test( 'Counter should increment', () => {
-  expect( 1 + 1 ).toBe(2)
+test( 'Counter should increment count', () => {
+  const { container } = render(<Counter />)
+  const button = container.firstChild
+  expect( button.textContent ).toBe( 'You clicked 0 times so far...' )
+  fireEvent.click(button)
+  expect( button.textContent ).toBe( 'You clicked 1 times so far...' )
 })
