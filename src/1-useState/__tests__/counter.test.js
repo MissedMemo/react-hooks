244bb981d3368badcf7b657e0e0aa56a0f2e868c
AndyL <missedmemo@gmail.com>
2019-01-30 22:10:29 -0800

    refactor as custom hook

--- src/1-useState/__tests__/counter.test.js
+++ src/1-useState/__tests__/counter.test.js
@@ -2,10 +2,24 @@ import React from 'react'
 import { render, fireEvent } from 'react-testing-library'
 import Counter from '../counter.jsx'
 
+test( 'Counter should default to zero', () => {
+  const { container } = render(<Counter />)
+  const button = container.firstChild
+  expect( button.textContent ).toBe( 'You\'ve clicked 0 times!' )
+})
+
 test( 'Counter should increment count', () => {
   const { container } = render(<Counter />)
   const button = container.firstChild
-  expect( button.textContent ).toBe( 'You clicked 0 times so far...' )
+  expect( button.textContent ).toBe( 'You\'ve clicked 0 times!' )
   fireEvent.click(button)
-  expect( button.textContent ).toBe( 'You clicked 1 times so far...' )
+  expect( button.textContent ).toBe( 'You\'ve clicked 1 times!' )
+})
+
+test( 'Counter should support setting an initial value', () => {
+  const { container } = render(<Counter initialValue={2} />)
+  const button = container.firstChild
+  expect( button.textContent ).toBe( 'You\'ve clicked 2 times!' )
+  fireEvent.click(button)
+  expect( button.textContent ).toBe( 'You\'ve clicked 3 times!' )
 })
