c5bb674b8804468ba248326d0a6937554f819753
AndyL <missedmemo@gmail.com>
2019-01-31 11:39:36 -0800

    get localstorage only on 1st render, set only on value change

--- src/2-useEffect/counter.jsx
+++ src/2-useEffect/counter.jsx
@@ -14,12 +14,18 @@ const Counter = () => {
 // refactor as CUSTOM hook, with passed component properties
 
 const useCounter = initialValue => {
-  const value = Number( localStorage.getItem('count') || initialValue )
+
+   // runs only on FIRST render! (vs. simple assignment, run on EVERY render )
+  const value = () => Number( localStorage.getItem('count') || initialValue )
+
   const [count, setCount] = useState( value )
   const increment = () => setCount( count +1 )
+
+  // optional array arg specifies values to watch for change, skipping actions otherwise
   useEffect( ()=> {
     localStorage.setItem( 'count', count )
-  })
+  }, [count] )
+
   return { count, increment }
 }
 
