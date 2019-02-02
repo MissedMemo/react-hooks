b16ef9033703df968c9c8865170e14dc5407d5fb
AndyL <missedmemo@gmail.com>
2019-02-01 17:25:19 -0800

    refactor tests

--- src/2-useEffect/__tests__/counter.test.js
+++ src/2-useEffect/__tests__/counter.test.js
@@ -2,39 +2,10 @@ import React from 'react'
 import { render, fireEvent } from 'react-testing-library'
 import Counter from '../counter.jsx'
 
-beforeEach( () => {
-  localStorage.removeItem('count')
-})
+const clearStorage = () => localStorage.removeItem('count')
 
-afterEach( () => {
-  localStorage.removeItem('count')
-})
-
-/*
-test( 'Counter should default to zero', () => {
-  const { container } = render(<Counter />)
-  const button = container.firstChild
-  expect( button.textContent ).toBe( 'You\'ve clicked 0 times!' )
-})
-
-test( 'Counter should increment count', () => {
-  const { container } = render(<Counter />)
-  const button = container.firstChild
-  expect( button.textContent ).toBe( 'You\'ve clicked 0 times!' )
-  fireEvent.click(button)
-  expect( button.textContent ).toBe( 'You\'ve clicked 1 times!' )
-})
-*/
-
-/*
-test( 'Counter should support setting an initial value', () => {
-  const { container } = render(<Counter initialValue={2} />)
-  const button = container.firstChild
-  expect( button.textContent ).toBe( 'You\'ve clicked 2 times!' )
-  fireEvent.click(button)
-  expect( button.textContent ).toBe( 'You\'ve clicked 3 times!' )
-})
-*/
+beforeAll( clearStorage )
+afterEach( clearStorage )
 
 test( 'reads and sets local storage', () => {
   localStorage.setItem('count', 18)
--- src/2-useEffect/counter.jsx
+++ src/2-useEffect/counter.jsx
@@ -1,18 +1,5 @@
 import React, { useState, useEffect } from 'react'
 
-/*
-const Counter = () => {
-
-  const [count, setCount] = useState(0)
-
-  const increment = () => setCount( count +1 )
-
-  return <button onClick={ increment }>{ `You clicked ${count} times so far...` }</button>
-}
-*/
-
-// refactor as CUSTOM hook, with passed component properties
-
 const useCounter = initialValue => {
 
    // runs only on FIRST render! (vs. simple assignment, run on EVERY render )
