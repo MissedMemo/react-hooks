a1533e3c5d142b46a536c8fde014261339d18cf6
AndyL <missedmemo@gmail.com>
2019-02-03 10:56:10 -0800

    apply emotion core (vs. styled) syntax

--- src/3-useRef/app.jsx
+++ src/3-useRef/app.jsx
@@ -1,8 +1,18 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 import React, { Fragment } from 'react'
 import Tilt from './tilt'
 
-export default () => <div className='centered'>
+const centered = css`
+  background-color: lightgreen;
+  padding: 16px;
+  max-width: 300px;
+  text-align: center;
+  margin: 0 auto;
+`
+
+export default () => <div css={centered}>
   <Tilt>
-    <div className='centered'>I'm Tilting...</div>
+    <div css={centered}>I'm Tilting...</div>
   </Tilt>
 </div>
