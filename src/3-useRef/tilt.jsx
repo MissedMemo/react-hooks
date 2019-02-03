4d3804916dc12849416444b004472582ea782121
AndyL <missedmemo@gmail.com>
2019-02-02 23:10:38 -0800

    apply simplest emotion CSS-In-JS style

--- src/3-useRef/tilt.jsx
+++ src/3-useRef/tilt.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+
+const TiltChild = styled.div`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
 
 const Tilt = props => <div className='tiltRoot'>
-  <div className='tiltChild'>
+  <TiltChild>
     { props.children }
-  </div>
+  </TiltChild>
 </div>
 
 export default Tilt
