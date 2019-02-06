392be7f14db439b632004c7a58738df9d43a2d08
AndyL <missedmemo@gmail.com>
2019-02-06 13:49:06 -0800

    fix subproject name reference error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import App from './5-stopwatch-useReducer/app'
+import App from './5-useReducer-stopwatch/app'
 
 render(
   <App />,
