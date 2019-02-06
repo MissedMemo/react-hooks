89b9fdd25fbd7f9f4fe622d0190e5b85590ee732
AndyL <missedmemo@gmail.com>
2019-02-06 10:27:53 -0800

    remove legacy stopwatch implementation

--- src/5-stopwatch-useReducer/app.jsx
+++ src/5-stopwatch-useReducer/app.jsx
@@ -1,8 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 import React from 'react'
-import Stopwatch1 from './stopwatch-legacy'
-import Stopwatch2 from './stopwatch'
+import Stopwatch from './stopwatch'
 
 const app = css`
   padding-top: 100px;
@@ -10,21 +9,6 @@ const app = css`
   justify-content: center;
 `
 
-const wrapper = css`
-  margin: 0 40px;
-  label {
-    display: block;
-    margin-bottom: 24px;
-  }
-`
-
 export default () => <div css={app}>
-  <div css={wrapper}>
-    <label>Class-based...</label>
-    <Stopwatch1 />
-  </div>
-  <div css={wrapper}>
-    <label>Hook-based...</label>
-    <Stopwatch2 />
-  </div>
+  <Stopwatch />
 </div>
